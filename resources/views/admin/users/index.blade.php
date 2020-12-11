3d624233f21484569eee97a39aefdea10621cd20
Marckus Balingit <philipfry32@gmal.com>
2020-12-11 19:12:13 +0800

    Push Request

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -46,8 +46,7 @@
                                         <td>{{ $row->role}}</td>
                                         <td>{{ $row->diffForHumans()}}</td>
                                         <td>
-                                            <button class="btn btn-outline-success btn-sm btnEditStudent"><i class="fa fa-edit"></i></button>
-                                            <button class="btn btn-outline-warning btn-sm"><i class="fa fa-user"></i></button>
+                                            <button class="btn btn-outline-success btn-sm btnEdit"><i class="fa fa-edit"></i></button>
                                             <button class="btn btn-outline-danger btn-sm"><i class="fa fa-trash"></i></button>
                                         </td>
                                     </tr>
@@ -123,6 +122,72 @@
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                     <button type="button" class="btn btn-primary">Save changes</button>
                 </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="modal-edit">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modal-title">Add User</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form action="" method="post">
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>First Name</label>
+                                    <input type="text" class="form-control" placeholder="First Name">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>Middle Name</label>
+                                    <input type="text" class="form-control" placeholder="Middle Name">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>Last Name</label>
+                                    <input type="text" class="form-control" placeholder="Last Name">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>Email</label>
+                                    <input type="email" class="form-control" placeholder="Email">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>Contact Number</label>
+                                    <input type="text" class="form-control" placeholder="Contact Number">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>Role</label>
+                                    <select name="role_id" class="form-control cbo" data-placeholder="Select Role">
+                                        <option value=""></option>
+                                        <option value="1">Admin</option>
+                                        <option value="2">Student</option>
+                                        <option value="3">Employer</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
